a4fb3ba4b47367af5b0a068d4e871848c800cd1d
Jmike-dev <gmikekamau@gmail.com>
2026-02-11 16:39:18 +0300

    removed dead code

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -127,43 +127,6 @@ export function LoginForm({
                             <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
                                 Or continue with
                             </FieldSeparator>
-                            {/* <Field className="grid grid-cols-2 gap-4">
-                                <Button
-                                    variant="outline"
-                                    type="button"
-                                    onClick={handleSubmit}
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                                            fill="currentColor"
-                                        />
-                                    </svg>
-                                    <span className="sr-only">
-                                        Login with Google
-                                    </span>
-                                </Button>
-                                <Button
-                                    variant="outline"
-                                    type="button"
-                                    onClick={handleSubmit}
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                        fill="currentColor"
-                                    >
-                                        <path d="M22 12.07C22 6.48 17.52 2 12 2S2 6.48 2 12.07c0 5.03 3.66 9.19 8.44 9.93v-7.03H7.9v-2.9h2.54V9.41c0-2.52 1.5-3.92 3.8-3.92 1.1 0 2.25.2 2.25.2v2.48h-1.27c-1.25 0-1.64.78-1.64 1.58v1.9h2.8l-.45 2.9h-2.35V22c4.78-.74 8.44-4.9 8.44-9.93z" />
-                                    </svg>
-
-                                    <span className="sr-only">
-                                        Login with facebook
-                                    </span>
-                                </Button>
-                            </Field> */}
                             <FieldDescription className="text-center">
                                 Don&apos;t have an account?{" "}
                                 <NavLink to="/signup"> Sign Up</NavLink>
